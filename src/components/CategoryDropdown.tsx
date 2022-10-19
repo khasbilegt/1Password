93d5b41a345c308399264de402173aaf9579a078
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2022-10-19 18:55:12 +0800

    remove execa, use useExec

--- src/components/CategoryDropdown.tsx
+++ src/components/CategoryDropdown.tsx
@@ -1,8 +1,5 @@
-import { execa } from "execa";
-import { useEffect } from "react";
-
 import { Icon, List } from "@raycast/api";
-import { useCachedState } from "@raycast/utils";
+import { useExec } from "@raycast/utils";
 
 import { Category, CategoryName } from "../types";
 import { getCategoryIcon } from "../utils";
@@ -10,22 +7,17 @@ import { getCategoryIcon } from "../utils";
 export const DEFAULT_CATEGORY = "null";
 
 export function CategoryDropdown({ onCategoryChange }: { onCategoryChange: (newCategory: string) => void }) {
-  const [categories, setCategories] = useCachedState<Category[]>("categories", []);
-
-  useEffect(() => {
-    execa("/usr/local/bin/op", ["item", "template", "list", "--format=json"]).then(({ stdout }) => {
-      const parsed = (JSON.parse(stdout) as Category[]).sort((a, b) =>
-        a.name == b.name ? 0 : a.name > b.name ? 1 : -1
-      );
-      setCategories(parsed);
-    });
-  }, []);
+  const { data: categories } = useExec("/usr/local/bin/op", ["item", "template", "list", "--format=json"], {
+    initialData: [],
+    parseOutput: ({ stdout }) =>
+      (JSON.parse(stdout) as Category[]).sort((a, b) => (a.name == b.name ? 0 : a.name > b.name ? 1 : -1)),
+  });
 
   return (
     <List.Dropdown defaultValue="null" onChange={onCategoryChange} tooltip="Select Category" storeValue>
       <List.Dropdown.Section title="Item Categories">
         <List.Dropdown.Item key={"000"} icon={Icon.AppWindowGrid3x3} title="All Categories" value={DEFAULT_CATEGORY} />
-        {categories.map((category) => (
+        {(categories || []).map((category) => (
           <List.Dropdown.Item
             key={category.uuid}
             icon={getCategoryIcon(category.name.replaceAll(" ", "_").toUpperCase() as CategoryName)}
--- src/components/PasswordList.tsx
+++ src/components/PasswordList.tsx
@@ -1,8 +1,5 @@
-import { execa } from "execa";
-import { useEffect, useState } from "react";
-
 import { ActionPanel, Color, Icon, List } from "@raycast/api";
-import { useCachedState } from "@raycast/utils";
+import { useCachedState, useExec } from "@raycast/utils";
 
 import { CopyToClipboard } from "./ActionCopyToClipboard";
 import { CategoryDropdown, DEFAULT_CATEGORY } from "./CategoryDropdown";
@@ -10,26 +7,18 @@ import { Item } from "../types";
 import { getCategoryIcon } from "../utils";
 
 export function PasswordList() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [items, setItems] = useState<Item[]>([]);
+  const default_params = ["item", "list", "--long", "--format=json"];
   const [category, setCategory] = useCachedState<string>("selected_cateogry", DEFAULT_CATEGORY);
-  const default_params = ["item", "list", "--vault", "cbajoutjh5pmff2c42skyywsdy", "--long", "--format=json"];
 
-  useEffect(() => {
-    setIsLoading(true);
-    execa(
-      "/usr/local/bin/op",
-      category === DEFAULT_CATEGORY ? default_params : [...default_params, `--categories=${category}`]
-    )
-      .then(({ stdout }) => {
-        const parsed = (JSON.parse(stdout) as Item[]).sort((a, b) =>
-          a.title == b.title ? 0 : a.title > b.title ? 1 : -1
-        );
-        setItems(parsed);
-      })
-      .catch(() => console.error("Handle error!!!"))
-      .finally(() => setIsLoading(false));
-  }, [category]);
+  const { data: items, isLoading } = useExec(
+    "/usr/local/bin/op",
+    category === DEFAULT_CATEGORY ? default_params : [...default_params, `--categories=${category}`],
+    {
+      initialData: [],
+      parseOutput: ({ stdout }) =>
+        (JSON.parse(stdout) as Item[]).sort((a, b) => (a.title == b.title ? 0 : a.title > b.title ? 1 : -1)),
+    }
+  );
 
   const onCategoryChange = (newCategory: string) => {
     category !== newCategory && setCategory(newCategory);
